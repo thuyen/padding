132a44dbe6a3f5a669b547f3469684bbc9c4270b
Thuyen Ngo <vanthuyen@gmail.com>
2018-06-07 08:37:32 -0700

    fix bug

--- F/ext.py
+++ F/ext.py
@@ -2,24 +2,27 @@ import torch
 from torch.autograd import Function
 import padding
 
-class Padding(Function):
-    def __init__(self, pad):
-        self.pad = pad
-
-    def forward(self, x):
-        if x.is_cuda():
-            return padding.padh_cpu_forward(x, self.pad)
+class PaddingFunction(Function):
+    @staticmethod
+    def forward(ctx, x, pad_h=1, pad_w=0, flag=False):
+        ctx.constant = pad_h, pad_w, flag
+        if not x.is_contiguous():
+            x = x.contiguous()
+        if x.is_cuda:
+            out = padding.padh_gpu_forward(x, pad_h, pad_w, flag)
         else:
-            return padding.padh_gpu_forward(x, self.pad)
+            out = padding.padh_cpu_forward(x, pad_h, pad_w, flag)
+        return out
 
-    def backward(self, grad_output):
-        if grad_output.is_cuda():
-            return padding.padh_gpu_backward(x, self.pad)
+    @staticmethod
+    def backward(ctx, grad_output):
+        pad_h, pad_w, flag = ctx.constant
+        if not grad_output.is_contiguous():
+            grad_output = grad_output.contiguous()
+        if grad_output.is_cuda:
+            out = padding.padh_gpu_backward(grad_output, pad_h, pad_w, flag)
         else:
-            return padding.padh_gpu_backward(x, self.pad)
+            out = padding.padh_cpu_backward(grad_output, pad_h, pad_w, flag)
+        return out, None, None
 
-def pad(x, p):
-    op = Padding(p)
-    return op(x)
-
-
+pad = PaddingFunction.apply
