aa4a68c37ef0eaad9840179c09f0631b8c1c48e5
Thuyen Ngo <vanthuyen@gmail.com>
2018-06-16 16:03:42 -0700

    add fuse op

--- src/fuses_gpu.cpp
+++ src/fuses_gpu.cpp
@@ -1,15 +1,5 @@
 #include <torch/torch.h>
 
-at::Tensor padh_cpu_forward(
-    const at::Tensor &X, // 3d image hwc
-    const int pad, const int padw, const bool b
-    );
-
-at::Tensor padh_cpu_backward(
-    const at::Tensor &X, // 3d image hwc
-    const int pad, const int padw, const bool b
-    );
-
 at::Tensor padh_gpu_forward(
     const at::Tensor &X, // 3d image hwc
     const int pad, const int padw, const bool b
@@ -18,18 +8,6 @@ at::Tensor padh_gpu_forward(
 at::Tensor padh_gpu_backward(
     const at::Tensor &X, // 3d image hwc
     const int pad, const int padw, const bool b
-    );
-
-at::Tensor crop_cpu_forward(
-    const at::Tensor &X, // nchw
-    const at::Tensor &R, // k2
-    const int pooled_height, const int pooled_width, const bool first
-    );
-
-at::Tensor crop_cpu_backward(
-    const at::Tensor &X, // nkchw
-    const at::Tensor &R, // k2
-    const int height, const int width, const bool first
     );
 
 at::Tensor crop_gpu_forward(
@@ -44,12 +22,26 @@ at::Tensor crop_gpu_backward(
     const int height, const int width, const bool first
     );
 
+
 at::Tensor conv2d_gpu_forward(
     const at::Tensor &X, // nchw
     const int pad, const int padw, const bool onesided,
     const at::Tensor weight, const at::Tensor bias,
     at::IntList stride, int64_t groups
-    );
+    ) {
+
+  auto& ctx = at::globalContext();
+
+  padded = padh_gpu_forward(X, padh, padw, onsided);
+  return at::cudnn_convolution(
+      padded, weight, bias,
+      {0, 0}, stride, {1, 1}, groups,
+      ctx.benchmarkCuDNN(), ctx.deterministicCuDNN());
+
+  //cudnn_convolution(self, weight, bias,
+  //    padding, stride, dilation, groups, benchmark, deterministic)
+}
+
 
 std::tuple<at::Tensor, at::Tensor, at::Tensor> conv2d_gpu_backward(
     const at::Tensor &input, // nchw
@@ -57,14 +49,36 @@ std::tuple<at::Tensor, at::Tensor, at::Tensor> conv2d_gpu_backward(
     const int pad, const int padw, const bool onesided,
     const at::Tensor weight, const at::Tensor bias,
     at::IntList stride, int64_t groups
-    );
+    ) {
+  //grad_input, grad_weight, grad_bias =
+  auto& ctx = at::globalContext();
+  auto ret =
+    at::cudnn_convolution_backward(input, grad_output, weight,
+        {0, 0}, stride, {1, 1}, groups,
+        ctx.benchmarkCuDNN(),
+        ctx.deterministicCuDNN(),
+        {true, true, true});
+  //cudnn_convolution_backward(self, grad_output, weight,
+  //    padding, stride, dilation, groups, benchmark, deterministic, std::array<bool,3> output_mask)
+  //
+  at::Tensor grad_input  = std::get<0>(ret);
+  at::Tensor grad_weight = std::get<1>(ret);
+  at::Tensor grad_bias   = std::get<2>(ret);
+  grad_input = padh_gpu_forward(grad_input, padh, padw, false);
+  return std::make_tuple(grad_input, grad_weight, grad_bias);
+}
+
 
 at::Tensor svf2d_gpu_forward(
     const at::Tensor &X, // nchw
     const at::Tensor &R, // k2
     const int pooled_height, const int pooled_width, const bool first,
     const at::Tensor &weight
-    );
+    ) {
+  auto cropped = crop_gpu_forward(X, R, pooled_height, pooled_width, false);
+  return at::matmul(cropped, weight);
+}
+
 
 at::Tensor svf2d_gpu_backward(
     const at::Tensor &grad_output // nchw
@@ -72,22 +86,10 @@ at::Tensor svf2d_gpu_backward(
     const at::Tensor &R, // k2
     const int pooled_height, const int pooled_width, const bool first,
     const at::Tensor &weight
-    );
+    ) {
+  auto grad_input   = at::matmul(grad_output, weight);
+  auto grad_weight = at::matmul(grad_output, X);
+  grad_input = crop_gpu_bacward(grad_input, pooled_height, pooled_width, false);
 
-
-PYBIND11_MODULE(_C, m) {
-  m.def("padh_cpu_forward",  &padh_cpu_forward,  "padh_cpu_forward");
-  m.def("padh_cpu_backward", &padh_cpu_backward, "padh_cpu_backward");
-  m.def("padh_gpu_forward",  &padh_gpu_forward,  "padh_gpu_forward");
-  m.def("padh_gpu_backward", &padh_gpu_backward, "padh_gpu_backward");
-
-  m.def("crop_cpu_forward",  &crop_cpu_forward,  "crop_cpu_forward");
-  m.def("crop_cpu_backward", &crop_cpu_backward, "crop_cpu_backward");
-  m.def("crop_gpu_forward",  &crop_gpu_forward,  "crop_gpu_forward");
-  m.def("crop_gpu_backward", &crop_gpu_backward, "crop_gpu_backward");
-
-  m.def("conv2d_gpu_forward",  &conv2d_gpu_forward,  "conv2d_gpu_forward");
-  m.def("conv2d_gpu_backward",  &conv2d_gpu_forward,  "conv2d_gpu_backward");
-  m.def("svf2d_gpu_forward", &svf2d_gpu_forward, "svf2d_gpu_fordward");
-  m.def("svf2d_gpu_backward", &svf2d_gpu_backward, "svf2d_gpu_backward");
+  return std::make_tuple(grad_input, grad_weight, grad_bias);
 }
